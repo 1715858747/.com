006a97f972c8bd5e32b579b5e84204ecab48b94b
1715858747 <47423795+1715858747@users.noreply.github.com>
2019-11-07 10:28:08 +0800

    复制数组或对象

--- extend.js
+++ extend.js
@@ -2,7 +2,7 @@
 //source代表被复制的对象或数组,target是接受复制数据的对象或数组
 function extend(source,target){
         for (var key in source) {
-            var value = source[key];
-            target[key] = value;
+            target[key] = source[key];
         }
-    }
+        return target;
+}
